f612ccf44caa72ab71e1f0051c212b3edbf76989
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-12-02 11:01:12 -0500

    Build package on new tag

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["redis>=3.0,<4.0",],
+    install_requires=["redis>=3.0,<4.0"],
 )
